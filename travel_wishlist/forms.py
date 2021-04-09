58d20d202aeda7ea50a517f0d48193f2b6fdb65f
John L <johnny@example.com>
2021-04-08 20:19:56 -0500

    Added data class in forms, fixed bugs place_detail

--- travel_wishlist/forms.py
+++ travel_wishlist/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.forms import FileInput, DateInput
 from .models import Place
 
 class NewPlaceForm(forms.ModelForm): # Form for the webpage
@@ -6,10 +7,16 @@ class NewPlaceForm(forms.ModelForm): # Form for the webpage
         model = Place
         fields = {'name', 'visited'}
 
+""" by default Django will show regular plain text field that accepts
+ text for a data field, con is no validation to see if they're entering
+  a real date. Reason why class is included here. """
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
 class TripReviewForm(forms.ModelForm):
     class Meta:
         model = Place
-        fields = {'notes', 'date_visited', 'photo'}
+        fields = ('notes', 'date_visited', 'photo')
         widgets = {
-            'date_visited': DateInput() # by default Django will show regular plain text field that accepts text for a data field, con is no validation to see if they're entering a real date
+            'date_visited': DateInput() 
         }
